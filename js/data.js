bca4e6211ae4b20f6c5c8e7a6c573b6256cb9698
danielags872-cell <daniel.ags872@gmail.com>
2026-05-02 13:07:51 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,25 +2,25 @@ let memories = [
   {
     text: "Nuestro concierto juntos 🎤",
     images: [
-      "assets/memories/concierto1.jpg",
-      "assets/memories/concierto2.jpg",
-      "assets/memories/concierto3.jpg"
+      "assets/memories/concierto1.jpeg",
+      "assets/memories/concierto2.jpeg",
+      "assets/memories/concierto3.jpeg"
     ],
     unlocked: false
   },
   {
     text: "Nosotros 💕",
     images: [
-      "assets/memories/ustedes1.jpg",
-      "assets/memories/ustedes2.jpg"
+      "assets/memories/ustedes1.jpeg",
+      "assets/memories/ustedes2.jpeg"
     ],
     unlocked: false
   },
   {
     text: "Mia 🐶",
     images: [
-      "assets/memories/mia1.jpg",
-      "assets/memories/mia2.jpg"
+      "assets/memories/mia1.jpeg",
+      "assets/memories/mia2.jpeg"
     ],
     unlocked: false
   }
