a4cb9e35606997bd6602fa95517d664fd340c663
danielags872-cell <daniel.ags872@gmail.com>
2026-05-02 12:09:15 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+function saveGame() {
+  localStorage.setItem("miaGame", JSON.stringify(memories));
+}
 
+function loadGame() {
+  let data = localStorage.getItem("miaGame");
+  if (data) memories = JSON.parse(data);
+}
+
+loadGame();
