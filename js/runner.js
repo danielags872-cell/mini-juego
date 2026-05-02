c6834697bca8b47b9d0c298c1e1e780c37f99257
danielags872-cell <daniel.ags872@gmail.com>
2026-05-02 13:20:40 -0500

    Update runner.js

--- js/runner.js
+++ js/runner.js
@@ -7,7 +7,12 @@ let obstacles = [];
 let gameInterval;
 
 function startRunner() {
+  console.log("Juego iniciado"); // <-- para que veas que sí corre
+
   obstacles = [];
+  player.y = 150;
+  player.vy = 0;
+
   clearInterval(gameInterval);
   gameInterval = setInterval(updateGame, 20);
 }
@@ -23,27 +28,41 @@ document.addEventListener("keydown", e => {
 function updateGame() {
   ctx.clearRect(0, 0, 400, 200);
 
+  // fondo
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, 400, 200);
+
+  // suelo
+  ctx.fillStyle = "#ddd";
+  ctx.fillRect(0, 170, 400, 30);
+
+  // jugador (ROSA grande para que lo veas)
   player.vy += gravity;
   player.y += player.vy;
 
   if (player.y > 150) player.y = 150;
 
-  ctx.fillRect(player.x, player.y, 20, 20);
+  ctx.fillStyle = "hotpink";
+  ctx.fillRect(player.x, player.y, 30, 30);
 
-  if (Math.random() < 0.02) {
+  // obstáculos (NEGROS grandes)
+  if (Math.random() < 0.03) {
     obstacles.push({ x: 400, y: 150 });
   }
 
+  ctx.fillStyle = "black";
   obstacles.forEach(o => {
-    o.x -= 4;
-    ctx.fillRect(o.x, o.y, 20, 20);
+    o.x -= 5;
+    ctx.fillRect(o.x, o.y, 30, 30);
 
+    // colisión
     if (
-      o.x < player.x + 20 &&
+      o.x < player.x + 30 &&
       o.x > player.x &&
       player.y > 130
     ) {
       clearInterval(gameInterval);
+      console.log("Choque"); // <-- debug
       unlockMemory();
     }
   });
